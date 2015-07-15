b3944ee64a05e516602261bd03ddc5f04fa24b9c
rain <lixinyu0476@163.com>
2015-07-15 19:48:42 +0800

    waterfall

--- html/js/common.js
+++ html/js/common.js
@@ -1,6 +1,47 @@
 
+//waterfall
+$(function(){
+	var oUrl = './data/data.json';
+	var container = $('.main-content');
+	var flag = true;
+	var ipage = 0;
+	console.log(1);
+	getData();
+	function getData(){
+		if (!flag) {
+			return;
+		};
+
+		$.getJSON(oUrl, {page:ipage}, function(data){
+			$.each(data, function(index, val) {
+				var els = '<img src="'+val.image+'" />';
+				container.append(els);
+			});
+			oHeight = container.height();
+			console.log('oHeight:'+oHeight);
+			initHeight = window.document.body.clientHeight;
+			window.document.body.clientHeight
+			if (oHeight < initHeight) {
+				getData();
+			};
+		})
+	}
+
+	$(window).on('scroll', function(event) {
+		console.log(window.document.body.clientHeight);
+		console.log($(window).height());
+		console.log(document.body.scrollHeight);
+		var totalHeight = $(window).scrollTop() + window.document.body.clientHeight + 300;
+		var contentHeight = document.body.scrollHeight;
+		console.log(totalHeight);
+		if (totalHeight >= contentHeight) {
+			getData();
+		};
+	});
+})
 
 
+//scroll top
 $(function(){
 	var els = '<div class="scroll"><span class="fa fa-chevron-up"></span><p>Top</p></div>';
 	$('body').append(els);
