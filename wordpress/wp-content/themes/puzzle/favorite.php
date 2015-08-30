8a51fb7cbe40a780eaad6b701dbf440c8681a263
rain <lixinyu0476@163.com>
2015-08-30 11:49:51 +0800

    save temp

--- wordpress/wp-content/themes/puzzle/favorite.php
+++ wordpress/wp-content/themes/puzzle/favorite.php
@@ -16,9 +16,28 @@ class Favorite
 		//初始化字段
 		//初始化cookie
 	}
-
 	/**
-	* 添加字段
+	* 构建数据库表
+	* 
+	* @param
+	* @return
+	* 
+	 */
+	function create_db_table() {
+		global $wpdb;
+		$table_name = $wpdb->prefix.'favorite';//表前缀
+		if ($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+			$sql = "CREATE TABLE ".$table_name."(
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				userid 
+				UNIQUE KEY id (id)
+			)ENGINE= MYISAM CHARACTER SET utf8;";
+		require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+		}
+	}	
+	/**
+	* 添加记录
 	* 
 	* @param
 	* @return
